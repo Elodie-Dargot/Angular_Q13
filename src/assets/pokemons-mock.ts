fc5988c0ee81430dc4347b5d8597f7abb94dc161
Elodie <elodie.dargot@gmail.com>
2023-10-04 07:54:00 +0200

    amélioration css

--- src/assets/pokemons-mock.ts
+++ src/assets/pokemons-mock.ts
@@ -2,23 +2,23 @@ import { Pokemon } from "src/app/shared/pokemon.model"
 
 export const pokemonMokeData: Pokemon[] = [
    {
-    "name": "Vespiquen",
-    "image": "https://images.pokemontcg.io/xy7/10.png",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus blandit ornare. Maecenas a lacus dapibus, pretium magna vitae, bibendum nunc. Curabitur urna ante, aliquet sit amet urna eu, mattis euismod urna. Morbi in nunc auctor, condimentum tellus id, ultrices libero. Aenean viverra lorem id nunc bibendum fringilla. Phasellus luctus lorem sed neque vestibulum, nec faucibus dui feugiat. In vel eleifend velit. Vestibulum rhoncus hendrerit congue. Nullam facilisis elit at felis rhoncus sagittis. Nullam consequat ac nunc ultrices iaculis. Etiam mauris nibh, luctus sit amet nisi non, fringilla fringilla libero. Sed dignissim gravida fermentum. Nullam malesuada mauris vitae justo tristique, ut gravida ante tristique. Ut elit sapien, lobortis id massa ac, commodo rhoncus nunc. "
+    "name": "Pikachu",
+    "image": "https://www.media.pokekalos.fr/img/pokemon/home/small/pikachu.png",
+    "description": "Pikachu (Pikachu en anglais et ピカチュウ en japonais) est un Pokémon de la 1ère génération de type electrik. Il est majoritairement de couleur Jaune. Il mesure 0.4 m (1.31 pieds) et pèse 6.0 kg (13.23 livres). Il fait partie de la catégorie des Pokémon Souris. Le Pokémon charge son corps en électricité statique, et tout contact avec lui peut paralyser. Paratonnerre (Talent caché) Le Pokémon détourne sur lui les capacités de type Électrik et les neutralise, tout en augmentant son Attaque Spéciale."
     },
     {
-    "name": "Cubone",
-    "image": "https://images.pokemontcg.io/dp6/90.png",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus blandit ornare. Maecenas a lacus dapibus, pretium magna vitae, bibendum nunc. Curabitur urna ante, aliquet sit amet urna eu, mattis euismod urna. Morbi in nunc auctor, condimentum tellus id, ultrices libero. Aenean viverra lorem id nunc bibendum fringilla. Phasellus luctus lorem sed neque vestibulum, nec faucibus dui feugiat. In vel eleifend velit. Vestibulum rhoncus hendrerit congue. Nullam facilisis elit at felis rhoncus sagittis. Nullam consequat ac nunc ultrices iaculis. Etiam mauris nibh, luctus sit amet nisi non, fringilla fringilla libero. Sed dignissim gravida fermentum. Nullam malesuada mauris vitae justo tristique, ut gravida ante tristique. Ut elit sapien, lobortis id massa ac, commodo rhoncus nunc. "
+    "name": "Salamèche",
+    "image": "https://www.media.pokekalos.fr/img/pokemon/home/small/salameche.png",
+    "description": "Salamèche (Charmander en anglais et ヒトカゲ en japonais) est un Pokémon de la 1ère génération de type feu. Il est majoritairement de couleur Rouge. Il mesure 0.6 m (1.97 pieds) et pèse 8.5 kg (18.74 livres). Il fait partie de la catégorie des Pokémon Lézard. Augmente la puissance des capacités de type Feu du Pokémon quand il a perdu une certaine quantité de PV. Quand le soleil brille, l'Attaque Spéciale du Pokémon augmente mais il perd des PV à chaque tour."
     },
     {
-    "name": "Lairon",
-    "image": "https://images.pokemontcg.io/ex16/32.png",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus blandit ornare. Maecenas a lacus dapibus, pretium magna vitae, bibendum nunc. Curabitur urna ante, aliquet sit amet urna eu, mattis euismod urna. Morbi in nunc auctor, condimentum tellus id, ultrices libero. Aenean viverra lorem id nunc bibendum fringilla. Phasellus luctus lorem sed neque vestibulum, nec faucibus dui feugiat. In vel eleifend velit. Vestibulum rhoncus hendrerit congue. Nullam facilisis elit at felis rhoncus sagittis. Nullam consequat ac nunc ultrices iaculis. Etiam mauris nibh, luctus sit amet nisi non, fringilla fringilla libero. Sed dignissim gravida fermentum. Nullam malesuada mauris vitae justo tristique, ut gravida ante tristique. Ut elit sapien, lobortis id massa ac, commodo rhoncus nunc. "
+    "name": "Bulbizarre",
+    "image": "https://www.media.pokekalos.fr/img/pokemon/home/small/bulbizarre.png",
+    "description": "Bulbizarre (Bulbasaur en anglais et フシギダネ en japonais) est un Pokémon de la 1ère génération de type plante et poison. Il est majoritairement de couleur Vert. Il mesure 0.7 m (2.3 pieds) et pèse 6.9 kg (15.21 livres).Engrais: Il fait partie de la catégorie des Pokémon Graine. Augmente la puissance des capacités de type Plante du Pokémon quand il a perdu une certaine quantité de PV. Chlorophylle (Talent caché): Augmente la Vitesse du Pokémon s'il y a du soleil."
     },
     {
-    "name": "Snorlax",
-    "image": "https://images.pokemontcg.io/xy0/26.png",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus blandit ornare. Maecenas a lacus dapibus, pretium magna vitae, bibendum nunc. Curabitur urna ante, aliquet sit amet urna eu, mattis euismod urna. Morbi in nunc auctor, condimentum tellus id, ultrices libero. Aenean viverra lorem id nunc bibendum fringilla. Phasellus luctus lorem sed neque vestibulum, nec faucibus dui feugiat. In vel eleifend velit. Vestibulum rhoncus hendrerit congue. Nullam facilisis elit at felis rhoncus sagittis. Nullam consequat ac nunc ultrices iaculis. Etiam mauris nibh, luctus sit amet nisi non, fringilla fringilla libero. Sed dignissim gravida fermentum. Nullam malesuada mauris vitae justo tristique, ut gravida ante tristique. Ut elit sapien, lobortis id massa ac, commodo rhoncus nunc. "
+    "name": "Magirêve",
+    "image": "https://www.media.pokekalos.fr/img/pokemon/home/small/magireve.png",
+    "description": "Magirêve (Mismagius en anglais et ムウマージ en japonais) est un Pokémon de la 4ème génération de type spectre. Il est majoritairement de couleur Violet. Il mesure 0.9 m (2.95 pieds) et pèse 4.4 kg (9.7 livres). Il fait partie de la catégorie des Pokémon Magique. Lévitation: Le Pokémon flotte, ce qui l'immunise contre les capacités de type Sol."
     },
 ]
